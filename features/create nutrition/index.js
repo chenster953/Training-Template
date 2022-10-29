566b90405e5bb17e69dc343614e170266707596f
James <zhaojun.chen0716@gmail.com>
2022-10-28 22:28:14 -0700

    SORT ORDER UPDDATE

--- features/create nutrition/index.js
+++ features/create nutrition/index.js
@@ -8,23 +8,56 @@ const highest = document.querySelector('.highest');
 
 let results = false;
 submit.addEventListener('click', ()=> {
-  results === true;
+  recipes.innerHTML = '';
+  results = true;
   fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=0b67ee8883dc43028b34e96eccabe5e7&query=${searchbox.value}&number=20&minCalories=${minCalories.value}&maxCalories=${maxCalories.value}`)
     .then((res) => res.json())
     .then((data) => {
+      let recipeList = [];
       const results = data.results;
-      console.log(results)
       results.forEach((result)=> {
-        const newRecipe = document.createElement('div');
-        newRecipe.innerHTML = `
-        <img class='image' src=${result.image}>
-        <h3 class='title'>${result.title}<h3/>
-        <p class='calories'>Calories: ${parseInt(result.nutrition.nutrients[0].amount)}<p/>`;
-        recipes.appendChild(newRecipe);
-      })
+        function Recipe(image, title, calories) {
+          this.image = image;
+          this.title = title;
+          this.calories = calories;
+        }
+        let newRecipe = new Recipe(result.image, result.title, parseInt(result.nutrition.nutrients[0].amount));
+        console.log(newRecipe);
+        recipeList.push(newRecipe);
+        const newRecipeDiv = document.createElement('div');
+        newRecipeDiv.innerHTML = `
+        <img class='image' src=${newRecipe.image}>
+        <h3 class='title'>${newRecipe.title}<h3/>
+        <p class='calories'>Calories: ${newRecipe.calories}<p/>`;
+        recipes.appendChild(newRecipeDiv);      
+        lowest.addEventListener('click', ()=> {
+          recipeList.sort((a,b)=> {
+            return a.calories - b.calories;
+          })
+          recipes.innerHTML = '';
+          for (i = 0; i < recipeList.length; i++) {
+            const lowestRecipeDiv = document.createElement('div');
+            lowestRecipeDiv.innerHTML = `
+            <img class='image' src=${recipeList[i].image}>
+            <h3 class='title'>${recipeList[i].title}<h3/>
+            <p class='calories'>Calories: ${recipeList[i].calories}<p/>`;
+            recipes.appendChild(lowestRecipeDiv);
+          }
+        })
+        highest.addEventListener('click', ()=> {
+          recipeList.sort((a,b)=> {
+            return b.calories - a.calories;
+          })
+          recipes.innerHTML = '';
+          for (i = 0; i < recipeList.length; i++) {
+            const highestRecipeDiv = document.createElement('div');
+            highestRecipeDiv.innerHTML = `
+            <img class='image' src=${recipeList[i].image}>
+            <h3 class='title'>${recipeList[i].title}<h3/>
+            <p class='calories'>Calories: ${recipeList[i].calories}<p/>`;
+            recipes.appendChild(highestRecipeDiv);
+          }
+        })
+      });
     })
 });
-
-if (results) {
-  
-}
