1062fb2ff5e2528c7f32b1db21e802129283c445
James <zhaojun.chen0716@gmail.com>
2022-10-31 19:38:51 -0700

    UPDATE SAVED RECIPES

--- features/create nutrition/index.js
+++ features/create nutrition/index.js
@@ -6,37 +6,59 @@ const recipes = document.querySelector('.recipes');
 const lowest = document.querySelector('.lowest');
 const highest = document.querySelector('.highest');
 const savedButton = document.querySelector('.saved');
+const dragging = document.querySelector('.dragging'); 
+
+let draggingElements;
+let draggingElementTitle;
+let draggingElementLink;
+let draggingElementImage;
+let draggingElementCalories;
 
 let savedRecipes = [];
-let results = false;
+function Recipe(image, title, calories, link, id) {
+  this.image = image;
+  this.title = title;
+  this.calories = calories;
+  this.link = link;
+  this.id = id
+}
+
 submit.addEventListener('click', ()=> {
   recipes.innerHTML = '';
-  results = true;
   fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=0b67ee8883dc43028b34e96eccabe5e7&query=${searchbox.value}&number=20&minCalories=${minCalories.value}&maxCalories=${maxCalories.value}&addRecipeInformation=true&addRecipeNutrition=true`)
     .then((res) => res.json())
     .then((data) => {
       let recipeList = [];
+      savedRecipes = [];
       const results = data.results;
       console.log(results)
       results.forEach((result)=> {
-        function Recipe(image, title, calories, link, id) {
-          this.image = image;
-          this.title = title;
-          this.calories = calories;
-          this.link = link;
-          this.id = id
-        }
         let newRecipe = new Recipe(result.image, result.title, parseInt(result.nutrition.nutrients[0].amount), result.spoonacularSourceUrl, result.id);
         recipeList.push(newRecipe);
         const newRecipeDiv = document.createElement('div');
         newRecipeDiv.innerHTML = `
         <img class='image' src=${newRecipe.image}>
         <h3 class='title'>${newRecipe.title}</h3>
-        <p class='calories'>Calories: ${newRecipe.calories}</p>
-        <button class='${newRecipe.id}'>+</button>`;
+        <p class='calories'>Calories: ${newRecipe.calories}</p>`;
+        newRecipeDiv.setAttribute('draggable', true);
+        newRecipeDiv.setAttribute('title', `${newRecipe.title}`);
+        newRecipeDiv.setAttribute('link', `${newRecipe.link}`);
+        newRecipeDiv.setAttribute('calories', `${newRecipe.calories}`);
+        newRecipeDiv.setAttribute('image', `${newRecipe.image}`);
         recipes.appendChild(newRecipeDiv);
         newRecipeDiv.addEventListener('click', ()=> {
           (window.open(`${newRecipe.link}`));
+        });
+        newRecipeDiv.addEventListener('dragstart',()=> {
+          newRecipeDiv.classList.add('dragging');
+          draggingElementTitle = newRecipeDiv.getAttribute('title');
+          draggingElementImage = newRecipeDiv.getAttribute('image');
+          draggingElementCalories = newRecipeDiv.getAttribute('calories');
+          draggingElementLink = newRecipeDiv.getAttribute('link');
+          draggingElements = [draggingElementTitle, draggingElementCalories, draggingElementImage, draggingElementLink]
+        });
+        newRecipeDiv.addEventListener('dragend',()=> {
+          newRecipeDiv.classList.remove('dragging');
         });
         lowest.addEventListener('click', ()=> {
           recipeList.sort((a,b)=> {
@@ -69,5 +91,32 @@ submit.addEventListener('click', ()=> {
           }
         });
       });
-    })
+    });
 });
+
+savedButton.addEventListener('dragover', (e)=> {
+  e.preventDefault();
+});
+savedButton.addEventListener('dragenter', ()=> {
+  savedButton.style.backgroundColor = 'red';
+});
+savedButton.addEventListener('dragleave', ()=> {
+  savedButton.style.backgroundColor = 'transparent';
+});
+savedButton.addEventListener('drop', ()=> {
+  savedButton.style.backgroundColor = 'transparent';
+  savedRecipes.push(draggingElements);
+});
+savedButton.addEventListener('click', ()=> {
+  recipes.innerHTML = ''
+  savedRecipes.forEach((recipe)=> {
+    const savedElements = document.createElement('div');
+    savedElements.innerHTML = `
+    <img class='image' src=${recipe[2]}>
+    <h3 class='title'>${recipe[0]}</h3>
+    <p class='calories'>Calories: ${recipe[1]}</p>
+    <a href=${recipe[3]}>Link</a>
+    `;
+    recipes.appendChild(savedElements);
+  })
+})
